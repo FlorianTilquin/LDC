6dace7dfc4993ee8f1a2f8cd2bb2b8dbb7107294
Florian Tilquin <ftilquin@gmail.com>
2018-01-13 11:07:33 +0000

    Cosmétique et correction de bug pour l'extraction de features

--- aec.py
+++ aec.py
@@ -1,16 +1,19 @@
 #! /usr/bin/env python3
 # -*- encoding:utf-8 -*-
 
-import numpy as np
-import numpy.random as rd
-from keras.layers import Dense
+from keras import regularizers
+from keras.layers import Dense, Dropout
 from keras.models import Sequential
 
 
 def create_model():
     model = Sequential()
-    model.add(Dense(256, input_dim=614, activation='relu'))
-    model.add(Dense(8, activation='relu'))
+    model.add(Dense(256, input_dim=614, activation='relu',
+                    kernel_regularizer=regularizers.l2(0.5)))
+    # model.add(Dropout(0.3))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    # model.add(Dropout(0.3))
     model.add(Dense(3, activation='softmax'))
     model.compile(loss='categorical_crossentropy',  optimizer='adam', metrics=['accuracy'])
     return model
--- feature_extraction_whdb.py
+++ feature_extraction_whdb.py
@@ -17,8 +17,7 @@ leagues = np.unique(League.as_matrix())
 n_league = len(leagues)
 dfleague = df.groupby('league_id')
 
-MEANED = False  # False va produire des erreurs à cause des Nan, à moins qu'on considère un algo
-# qui gère les missings values, genre EM
+MEANED = False
 
 
 def mean_up(ex, n_m, goals):
@@ -30,112 +29,111 @@ def diff_to_pt(d):
     return 3 * (d > 0) if d != 0 else 1
 
 
+team_ft = ['gfh', 'gah', 'n_h', 'gfa', 'gaa', 'n_a', 'm_pt']
+n_ft = len(team_ft)
+
+
 def id_ft(ft):
     return team_ft.index(ft)
 
 
+def team_features_update(Team_ft, date, Tid, htg, atg, pts, HOME):
+    per = slice(date, date + 100)
+    if erase:
+        Team_ft[per, Tid, :] = 0
+    for ft in team_ft:
+        globals()[ft] = Team_ft[date, Tid, id_ft(ft)]
+    if HOME:
+        n_h += 1
+        Team_ft[per, Tid, id_ft('n_h')] = n_h
+        Team_ft[per, Tid, id_ft('gfh')] = mean_up(gfh, n_h, htg)
+        Team_ft[per, Tid, id_ft('gah')] = mean_up(gah, n_h, atg)
+    else:
+        n_a += 1
+        Team_ft[per, Tid, id_ft('n_a')] = n_a
+        Team_ft[per, Tid, id_ft('gfa')] = mean_up(gfa, n_a, atg)
+        Team_ft[per, Tid, id_ft('gaa')] = mean_up(gaa, n_a, htg)
+    n_match = n_a + n_h + 1
+    Team_ft[per, Tid, id_ft('m_pt')] = mean_up(m_pt, n_match, pts * (2 * HOME - 1))
+    return Team_ft
+
+
 Score = {}
 M_ft = {}
-for league in leagues:
-    dfl = dfleague.get_group(league)
-    dfl.sort_values(by='date', inplace=True)
-    dfl.reset_index(inplace=True)
 
-    Score[str(league)] = dfl.loc[:,
-                                 'home_team_goal':'away_team_goal'].as_matrix()
-    BM = dfl.loc[:, 'B365H':'BSA'].as_matrix()
-    BM.shape
-    BM = BM.reshape(-1, 10, 3)
 
-    teams = np.unique(dfl.home_team_api_id).tolist()
-    n_team = len(teams)
-    dates = np.unique(dfl.date).tolist()
-    n_date = len(dates)
-    seasons = np.unique(dfl.season)
-    n_season = len(seasons)
+def extract_match_features(leagues='all', MEANED=False, dyn_length=20, Book=True):
+    for league in leagues:
+        dfl = dfleague.get_group(league)
+        dfl.sort_values(by='date', inplace=True)
+        dfl.reset_index(inplace=True)
 
-    team_ft = ['gfh', 'gah', 'n_h', 'gfa', 'gaa', 'n_a', 'm_pt']
-    n_ft = len(team_ft)
-    Team_ft = np.zeros((n_date, n_team, n_ft))
-    Match_ft = np.zeros(
-        (len(dfl), 2 * (n_ft + 5 + 28 * (10 - 9 * MEANED)) + 30))
+        Score[str(league)] = dfl.loc[:,
+                                     'home_team_goal':'away_team_goal'].as_matrix()
+        if Book:
+            BM = dfl.loc[:, 'B365H':'BSA'].as_matrix()
+            BM = BM.reshape(-1, 10, 3)
 
-    prev_seas = dfl.season[0]
-    k = 0
-    for i in range(len(dfl)):
-        date = dates.index(dfl.date[i])
-        print("features for day ", dfl.date[i], ", league:", league)
-        home_team = teams.index(dfl.home_team_api_id[i])
-        away_team = teams.index(dfl.away_team_api_id[i])
-        cur_seas = dfl.season[i]
-        if cur_seas != prev_seas:
-            k = 0
-        if k < len(np.unique(dfl.loc[dfl.season == dfl.season[0], 'home_team_api_id'])) / 2:
-            # Ça devrait fonctionner a priori ?
-            Team_ft[date:, home_team, :] = 0
-            # Les 10 premiers matchs de la saison correspondent forcément à la 1re journée de L1
-            Team_ft[date:, away_team, :] = 0
-            k = k + 1
-        prev_seas = cur_seas
+        teams = np.unique(dfl.home_team_api_id).tolist()
+        dates = np.unique(dfl.date).tolist()
+        seasons = np.unique(dfl.season)
 
-        htg = df.home_team_goal[i]
-        atg = df.away_team_goal[i]
-        dtg = htg - atg
+        Team_ft = np.zeros((len(dates), len(teams), n_ft))
+        Match_ft = np.zeros(
+            (len(dfl), 2 * (n_ft + 5 + 28 * (10 - 9 * MEANED)) + 30))
 
-        # HT Features update
-        m_p = Team_ft[date, home_team, id_ft('m_pt')]
-        pts = diff_to_pt(dtg)
-        n_home = Team_ft[date, home_team, id_ft('n_h')] + 1
-        Team_ft[date:, home_team, id_ft('n_h')] = n_home
+        prev_seas = dfl.season[0]
+        k = 0
+        for i in range(len(dfl)):
+            date = dates.index(dfl.date[i])
+            if date == 0:
+                continue
 
-        n_match = Team_ft[date, home_team, id_ft(
-            'n_h')] + Team_ft[date, home_team, id_ft('n_a')]
-        Team_ft[date:, home_team, id_ft('m_pt')] = mean_up(m_p, n_match, pts)
+            #  Add Bookmaker features
+            bm = BM[i, ...]
+            bm[np.any(np.isnan(bm), 1), :] = 1 / np.array([0.4587, 0.2539, 0.2874])
+            Match_ft[i, -30:] = bm.reshape(1, -1)
 
-        gfh = Team_ft[date, home_team, id_ft('gfh')]
-        gah = Team_ft[date, home_team, id_ft('gah')]
-        Team_ft[date:, home_team, id_ft('gfh')] = mean_up(gfh, n_home, htg)
-        Team_ft[date:, home_team, id_ft('gah')] = mean_up(gah, n_home, atg)
+            print("features for day ", dfl.date[i], ", league:", league)
+            home_team = teams.index(dfl.home_team_api_id[i])
+            away_team = teams.index(dfl.away_team_api_id[i])
+            cur_seas = dfl.season[i]
+            if cur_seas != prev_seas:
+                k = 0
+            if k < len(np.unique(dfl.loc[dfl.season == dfl.season[0], 'home_team_api_id'])) / 2:
+                erase = True
+                k = k + 1
+            prev_seas = cur_seas
 
-        # AT Features update
-        m_p = Team_ft[date, away_team, id_ft('m_pt')]
-        pts = diff_to_pt(dtg)
-        n_away = Team_ft[date, away_team, id_ft('n_a')] + 1
-        Team_ft[date:, away_team, id_ft('n_a')] = n_away
+            # Team Features update
+            htg = df.home_team_goal[i]
+            atg = df.away_team_goal[i]
+            dtg = htg - atg
+            pts = diff_to_pt(dtg)
+            Team_ft = team_features_update(Team_ft, date, home_team, htg, atg, pts, erase, True)
+            Team_ft = team_features_update(Team_ft, date, away_team, htg, atg, pts, erase, False)
 
-        n_match = Team_ft[date, away_team, id_ft(
-            'n_h')] + Team_ft[date, away_team, id_ft('n_a')]
-        Team_ft[date:, away_team, id_ft('m_pt')] = mean_up(m_p, n_match, pts)
-
-        gfa = Team_ft[date, away_team, id_ft('gfa')]
-        gaa = Team_ft[date, away_team, id_ft('gaa')]
-        Team_ft[date:, away_team, id_ft('gfa')] = mean_up(gfa, n_away, atg)
-        Team_ft[date:, away_team, id_ft('gaa')] = mean_up(gaa, n_away, htg)
-
-        # Match ft. filling
-        if date > 0:
+            # Match ft. filling
             Match_ft[i, :n_ft] = Team_ft[date - 1, home_team, :]
             Match_ft[i, n_ft:2 * n_ft] = Team_ft[date - 1, away_team, :]
             # Match_ft[i, 2 * n_ft:] = BM[i, ...].reshape(1, -1)
 
-        #  Add players features (33 per player, thus 5+28*10 if MEANED=False else 33) to the list
-        player_list_id = dfl.loc[i,
-                                 'home_player_1':'away_player_11'].as_matrix()
-        ht_feat = get_team_feat(
-            player_list_id[:11], pd.Series(dfl.date[i]), MEANED)
-        at_feat = get_team_feat(
-            player_list_id[11:], pd.Series(dfl.date[i]), MEANED)
-        Match_ft[i, 2 * n_ft:2 * n_ft + 5 + 28 * (10 - 9 * MEANED)] = ht_feat
-        Match_ft[i, 2 * n_ft + 5 + 28 * (10 - 9 * MEANED):2 *
-                 (n_ft + 5 + 28 * (10 - 9 * MEANED))] = at_feat
+            #  Add players features (33 per player: 5GK + 28 field) to the list
+            player_list_id = dfl.loc[i,
+                                     'home_player_1':'away_player_11'].as_matrix()
+            ht_feat = get_team_feat(
+                player_list_id[:11], pd.Series(dfl.date[i]), MEANED)
+            at_feat = get_team_feat(
+                player_list_id[11:], pd.Series(dfl.date[i]), MEANED)
+            Match_ft[i, 2 * n_ft:2 * n_ft + 5 + 28 * (10 - 9 * MEANED)] = ht_feat
+            Match_ft[i, 2 * n_ft + 5 + 28 * (10 - 9 * MEANED):2 *
+                     (n_ft + 5 + 28 * (10 - 9 * MEANED))] = at_feat
 
-        #  Add Bookmaker features
-        bm = BM[i, ...]
-        bm[np.any(np.isnan(bm), 1), :] = 1 / np.array([0.4587, 0.2539, 0.2874])
-        Match_ft[i, -30:] = bm.reshape(1, -1)
+        M_ft[str(league)] = Match_ft
+        del dfl
 
-    M_ft[str(league)] = Match_ft
-    del dfl
+    return M_ft, Score
+
 
 pkl.dump(M_ft, open('M_ft.p', 'wb'))
 pkl.dump(Score, open('Score.p', 'wb'))
